7832ef63b2f84af94abab4122bf27d4071f80983
Danilo Pianini <danilo.pianini@unibo.it>
2020-09-04 12:19:03 +0200

    Add chromium as a second renderer

--- slides/makepdf.rb
+++ slides/makepdf.rb
@@ -1,18 +1,42 @@
 #!/usr/bin/env ruby
 
 # Configuration options
-website_address = 'http://localhost:1313/Course-Laboratory-of-Software-Systems/' # always add the trailing slash
-decktape_options = '-s 1440x900 -p 20'
+
+class Generator
+    def generate(source = '', suffix = '', base_url = 'http://localhost:1313/Course-Laboratory-of-Software-Systems', options = '')
+        pdf_name = "#{(source.empty? ? '00-introduction' : source).gsub('/', '-') || ''}_#{suffix}.pdf"
+        puts "Using #{self.class.name} to generate #{pdf_name}.pdf from /#{source}"
+        generation_command(
+            "#{base_url}/#{source}",
+            pdf_name,
+            options
+        )
+    end
+end
+class DecktapeGenerator < Generator
+    def generation_command(url, file_name, options)
+        options += '-s 1440x900 -p 20'
+        puts "url: #{url}, file_name: #{file_name}, options: #{options}"
+        command = "decktape #{options} #{url} #{file_name}"
+        puts "Launching: #{command}"
+        puts `#{command}`
+    end
+end
+class ChromiumGenerator < Generator
+    def generation_command(url, file_name, options)
+        options += '--disable-gpu --virtual-time-budget=10000 --window-size=1440,900'
+        command = "chromium --headless --run-all-compositor-stages-before-draw #{options} --print-to-pdf=#{file_name} #{url}?print-pdf"
+        puts "Launching: #{command}"
+        puts `#{command}`
+    end
+end
+
+chromium = ChromiumGenerator.new
+decktape = DecktapeGenerator.new
 
 # Workaround for decktape bug
 puts 'Working around bug: https://github.com/astefanutti/decktape/issues/220'
 puts 'By replacing max vieport relative sizes 95/80 with 120/140'
-
-shortcode_path = 'layouts/shortcodes/image.html'
-custom_image_shortcode = File.read(shortcode_path)
-replace_width = custom_image_shortcode.gsub(/}}\s*95\s*{{/, '}}120{{')
-replace_both = replace_width.gsub(/}}\s*80\s*{{/, '}}140{{')
-File.open(shortcode_path, 'w') { | file | file.puts replace_both }
 
 log_file_name = "hugo.log"
 `touch #{log_file_name}`
@@ -34,19 +58,13 @@ until log_file.gets =~ /.*Watching for config changes in.*/
 end
 puts "Generation complete!"
 
-def generate_slides(decktape_command, target)
-    proposed_name = target&.gsub('/', '-') || ""
-    pdf_name = proposed_name&.empty? ? "00-introduction" : proposed_name
-    puts "Using decktape to generate #{pdf_name}.pdf from /#{target}"
-    `#{decktape_command}#{target} #{pdf_name}.pdf`
+# Site has been generated and is being served. Launch generation
+
+for pack in [''] + contents do
+    decktape.generate(pack, 'decktape')
+    chromium.generate(pack, 'chromium')
 end
 
-decktape_command_base = "decktape #{decktape_options} #{website_address}"
-for pack in contents
-    generate_slides(decktape_command_base, pack)
-end
-# Site has been generated and is being served. Launch decktape
-puts `#{decktape_command_base} slides.pdf`
 # Terminate the server
 Process.kill("TERM", server)
 begin
@@ -55,8 +73,3 @@ rescue SystemCallError
     puts "The server terminated before I could wait for it."
 end
 File.delete(log_file)
-
-# Undo the bug workaround
-puts 'Undoing the bug workaround'
-File.open(shortcode_path, 'w') { | file | file.puts custom_image_shortcode }
-puts 'Done.'
