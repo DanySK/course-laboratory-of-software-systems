705489437eb48b83faaec21fbdf54391d59cebf6
Danilo Pianini <danilo.pianini@gmail.com>
2020-09-22 14:48:14 +0200

    Stub a java compilation task

--- code/03-automation/testdeps/build.gradle.kts
+++ code/03-automation/testdeps/build.gradle.kts
@@ -1,21 +1,45 @@
-// declare a "configuration" named "someConfiguration"
-val someConfiguration by configurations.creating
+import org.apache.tools.ant.taskdefs.condition.Os
+import org.gradle.internal.jvm.Jvm
+
+// Create a new configuration, our compileClasspath
+val compileClasspath by configurations.creating
 
 dependencies {
-    // add a project dependency to the "someConfiguration" configuration
-    someConfiguration(project(":lib"))
+    // Search libraries in lib
+    val libraries = projectDir
+        .listFiles { it: File -> it.isDirectory && it.name == "lib"}
+        ?.firstOrNull()
+        ?.listFiles { it: File -> it.extension == "jar" }
+        ?: emptyArray()
+    println("Found libraries: ${libraries.joinToString()}")
+    // Add them to the classpath
+    compileClasspath(files(*libraries))
 }
 
-// Create a configuration for the compilation,
-// a dependency with a jar file,
-// a compilation task
-// and compile a simple Kotlin (or Java) file
-
-tasks.register("hello") {
-    doLast{ println("Hello") }
+// Write a task of type Exec that launches javac
+tasks.register<Exec>("compileJava") {
+    // Resolve the classpath configuration
+    // (in general, files could be remote and need fetching)
+    val classpathFiles = compileClasspath.resolve()
+    // Build the command
+    val separator = if (Os.isFamily(Os.FAMILY_WINDOWS)) ";" else ":"
+    val sources = projectDir
+        .listFiles { it: File -> it.isDirectory && it.name == "src" }
+        ?.firstOrNull()
+        ?.walk()
+        ?.filter { it.extension == "java" }
+        ?.map { it.absolutePath }
+        ?.toList()
+        ?.toTypedArray()
+    if (sources != null)  {
+        // Use the current JVM's javac
+        val javacExecutable = Jvm.current().javacExecutable.absolutePath
+        commandLine(
+            "$javacExecutable",
+            "-cp", classpathFiles.joinToString(separator = separator),
+            "-d", "bin",
+            *sources
+        )
+    }
+    // the task's doLast is inherited from Exec
 }
-
-dependencies {
-
-}
-
