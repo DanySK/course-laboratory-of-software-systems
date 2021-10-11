b03aca493eb7215b55f7c5bec24acd9f7c43cf5c
Danilo Pianini <danilo.pianini@unibo.it>
2021-10-11 16:59:15 +0200

    Prepare a full integrity check

--- code/automation/integrity_check.sh
+++ code/automation/integrity_check.sh
@@ -1,13 +1,56 @@
 #!/bin/sh
-cd 00-configuration-execution
+cd 00*
 gradle helloWorld
 cd ..
 
-cd 01-task-type
+cd 01*
 gradle printJavaVersion
 cd ..
 
-cd 02-java-plain-compile
+cd 02*
 gradle compileJava
 cd ..
 
+cd 03*
+gradle compileJava
+cd ..
+
+cd 04*
+gradle runJava
+cd ..
+
+cd 05*
+gradle clean runJava
+cd ..
+
+cd 06*
+gradle clean compileJava
+cd ..
+
+cd 07*
+gradle clean compileJava
+cd ..
+
+cd 08*
+gradle clean compileJava
+cd ..
+
+cd 09*
+gradle build
+cd ..
+
+cd 10*
+gradle build
+cd ..
+
+cd 11*
+gradle build
+cd ..
+
+cd 12*
+gradle build
+cd ..
+
+cd 13*
+gradle build
+cd ..
